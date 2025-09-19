0bf590129e92a3238a570a4c6fb37bf2be0b7f4b
Yancarlos <yancarlos_cartagena82171@elpoli.edu.co>
2025-09-19 11:30:15 -0500

    Refactor App component structure and enhance styling in App.css; add VSCode settings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,27 @@
+
 import './App.css';
 import ReporteClientesPorcinos from './ReporteClientesPorcinos';
 import ClientesCRUD from './ClientesCRUD';
 import AlimentacionCRUD from './AlimentacionCRUD';
 import PorcinosCRUD from './PorcinosCRUD';
 
+
 function App() {
   return (
     <div>
       <h1>Granja S.A.</h1>
-    <ClientesCRUD />
-    <hr />
-    <AlimentacionCRUD />
-    <hr />
-    <PorcinosCRUD />
-    <hr />
-    <ReporteClientesPorcinos />
+      <section>
+        <ClientesCRUD />
+      </section>
+      <section>
+        <AlimentacionCRUD />
+      </section>
+      <section>
+        <PorcinosCRUD />
+      </section>
+      <section>
+        <ReporteClientesPorcinos />
+      </section>
     </div>
   );
 }
